e1cd22d322aa51f5b8eb8e0bc50e24e186e0cade
Guljeren <guljeren7777@gmail.com>
2026-05-24 16:04:27 +0300

    fix: идеальный стиль кода для линтера

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import date
+
 
 @dataclass
 class Transaction:
--- app/io/readers.py
+++ app/io/readers.py
@@ -3,19 +3,21 @@ import json
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import List, Dict, Type
-from app.core.models import Transaction
 from app.core.exceptions import DataFormatError
+
 
 class BaseReader(ABC):
     """Абстрактный базовый класс для всех читателей файлов."""
-    
+
     @abstractmethod
     def read(self, file_path: Path) -> List[Dict]:
         """Метод, который должен реализовать каждый дочерний класс."""
         pass
 
+
 class CSVReader(BaseReader):
     """Класс для чтения данных из CSV файлов."""
+
     def read(self, file_path: Path) -> List[Dict]:
         try:
             with open(file_path, mode='r', encoding='utf-8') as f:
@@ -24,14 +26,17 @@ class CSVReader(BaseReader):
         except Exception as e:
             raise DataFormatError(f"Ошибка в структуре CSV: {e}")
 
+
 class JSONReader(BaseReader):
     """Класс для чтения данных из JSON файлов."""
+
     def read(self, file_path: Path) -> List[Dict]:
         try:
             with open(file_path, mode='r', encoding='utf-8') as f:
                 return json.load(f)
         except Exception as e:
             raise DataFormatError(f"Ошибка в структуре JSON: {e}")
+
 
 # --- REGISTRY PATTERN ---
 # Этот словарь связывает расширение файла с нужным классом
@@ -40,12 +45,13 @@ READER_REGISTRY: Dict[str, Type[BaseReader]] = {
     '.json': JSONReader
 }
 
+
 def get_reader(file_path: Path) -> BaseReader:
     """Фабричная функция для получения нужного ридера."""
-    extension = file_path.suffix.lower() # Получаем .csv или .json
+    extension = file_path.suffix.lower()  # Получаем .csv или .json
     reader_class = READER_REGISTRY.get(extension)
-    
+
     if not reader_class:
         raise DataFormatError(f"Формат {extension} не поддерживается")
-    
-    return reader_class() # Создаем объект класса (экземпляр)
+
+    return reader_class()  # Создаем объект класса (экземпляр)
